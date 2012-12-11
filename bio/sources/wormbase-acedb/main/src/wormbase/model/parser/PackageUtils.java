ba2032aeb6976f7a7b229323e13c20212b8211c4
JD Wong <jdmswong@wormbase.org>
2012-12-11 14:13:30 -0500

    added XML sanitation function to PackageUtils

--- bio/sources/wormbase-acedb/main/src/wormbase/model/parser/PackageUtils.java
+++ bio/sources/wormbase-acedb/main/src/wormbase/model/parser/PackageUtils.java
@@ -5,6 +5,9 @@ package wormbase.model.parser;
 
 import java.io.*;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.w3c.dom.Document;
 import javax.xml.parsers.*;
@@ -64,5 +67,42 @@ public class PackageUtils {
 	    Document doc = builder.parse(is);
 	    is.close();
 	    return doc;
+	}
+	
+	/**
+	 * Removes numbers from the beginnings of XML tags in the string.
+	 * Ex: <2_point> -> <two_point>
+	 * @param xml
+	 * @return
+	 */
+	public String sanitizeXMLTags(String xml){
+		String repairedXML = new String();
+		
+		HashMap<Integer, String> numberMap = new HashMap<Integer, String>();
+		numberMap.put(1, "one");
+		numberMap.put(2, "two");
+		numberMap.put(3, "three");
+		numberMap.put(4, "four");
+		numberMap.put(5, "five");
+		numberMap.put(6, "six");
+		numberMap.put(7, "seven");
+		numberMap.put(8, "eight");
+		numberMap.put(9, "nine");
+		numberMap.put(0, "zero");
+
+		String patternStr = "(</?)(\\d)";
+		
+		Pattern pattern = Pattern.compile(patternStr);
+		Matcher matcher = pattern.matcher(xml);
+		StringBuffer sb = new StringBuffer();
+		while(matcher.find()){
+			String digit = matcher.group(2);
+			String processedText = numberMap.get(Integer.parseInt(digit));
+			matcher.appendReplacement(sb, matcher.group(2)+processedText);
+		}
+		matcher.appendTail(sb);
+
+		
+		return repairedXML;
 	}
 }
